c3345e7cb189ff966ed1e009e37d3ede976b05f3
Константин Бедрицкий <kmb2@tpu.ru>
2020-06-26 22:25:06 +0600

    Kyu 6 - Decode the Morse code

---
+++ src/kyu_6/decode_the_morse_code.rs
@@ -0,0 +1,50 @@
+// Decode the Morse code
+// https://www.codewars.com/kata/54b724efac3d5402db00065e/train/rust
+
+use std::collections::HashMap;
+
+struct MorseDecoder {
+    morse_code: HashMap<String, String>,
+}
+
+impl MorseDecoder {
+    fn new() -> Self {
+        let mut map = HashMap::new();
+        map.insert("....".to_string(), "H".to_string());
+        map.insert(".".to_string(), "E".to_string());
+        map.insert("-.--".to_string(), "Y".to_string());
+        map.insert("   ".to_string(), " ".to_string());
+        map.insert(".---".to_string(), "J".to_string());
+        map.insert("..-".to_string(), "U".to_string());
+        map.insert("-..".to_string(), "D".to_string());
+        map.insert(".".to_string(), "E".to_string());
+        Self { morse_code: map }
+    }
+    fn decode_morse(&self, encoded: &str) -> String {
+        encoded
+            .trim()
+            .split("   ")
+            .map(|s| {
+                s.split(" ")
+                    .filter_map(|x| self.morse_code.get(x))
+                    .cloned()
+                    .collect::<String>()
+            })
+            .collect::<Vec<_>>()
+            .join(" ")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hey_jude() {
+        let decoder = MorseDecoder::new();
+        assert_eq!(
+            decoder.decode_morse(".... . -.--   .--- ..- -.. ."),
+            "HEY JUDE"
+        );
+    }
+}
--- src/kyu_6/mod.rs
+++ src/kyu_6/mod.rs
@@ -1,2 +1,3 @@
 mod binaries;
+mod decode_the_morse_code;
 mod roboScript_1_implement_syntax_highlighting;
