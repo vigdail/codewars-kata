5b5aa9fe400bcf153fce1d0ff880818a0d938349
kmb2 <kmb2@tpu.ru>
2021-12-06 14:53:20 +0600

    From trait implementation for Token

--- src/kuy_2/symbolic_differentiation_of_prefix_expressions.rs
+++ src/kuy_2/symbolic_differentiation_of_prefix_expressions.rs
@@ -28,6 +28,30 @@ enum Token {
     Expression(Vec<Token>),
 }
 
+impl From<i32> for Token {
+    fn from(n: i32) -> Self {
+        Token::Number(n)
+    }
+}
+
+impl From<BinOp> for Token {
+    fn from(op: BinOp) -> Self {
+        Token::BinOp(op)
+    }
+}
+
+impl From<Fun> for Token {
+    fn from(fun: Fun) -> Self {
+        Token::Fun(fun)
+    }
+}
+
+impl From<Vec<Token>> for Token {
+    fn from(expr: Vec<Token>) -> Self {
+        Token::Expression(expr)
+    }
+}
+
 impl ToString for Token {
     fn to_string(&self) -> String {
         match self {
@@ -83,9 +107,11 @@ impl Diff for Token {
             Token::BinOp(op) => op.diff(tokens),
             Token::Fun(fun) => fun.diff(tokens),
             Token::Var => Token::Number(1),
-            Token::Expression(expr) => {
-                Token::Expression(expr.iter().map(|t| t.diff(tokens)).collect())
-            }
+            Token::Expression(expr) => expr
+                .iter()
+                .map(|t| t.diff(tokens))
+                .collect::<Vec<_>>()
+                .into(),
         }
     }
 }
@@ -101,44 +127,29 @@ impl Diff for BinOp {
         let left = tokens.pop().unwrap();
         let right = tokens.pop().unwrap();
         match self {
-            BinOp::Add => Token::Expression(vec![
-                Token::BinOp(BinOp::Add),
-                left.diff(tokens),
-                right.diff(tokens),
-            ]),
-            BinOp::Sub => Token::Expression(vec![
-                Token::BinOp(BinOp::Sub),
-                left.diff(tokens),
-                right.diff(tokens),
-            ]),
-            BinOp::Mul => Token::Expression(vec![
-                Token::BinOp(BinOp::Add),
-                Token::Expression(vec![
-                    Token::BinOp(BinOp::Mul),
-                    left.clone(),
-                    right.diff(tokens),
-                ]),
-                Token::Expression(vec![Token::BinOp(BinOp::Mul), left.diff(tokens), right]),
-            ]),
-            BinOp::Div => Token::Expression(vec![
-                Token::BinOp(BinOp::Div),
+            BinOp::Add => vec![BinOp::Add.into(), left.diff(tokens), right.diff(tokens)].into(),
+            BinOp::Sub => vec![BinOp::Sub.into(), left.diff(tokens), right.diff(tokens)].into(),
+            BinOp::Mul => vec![
+                BinOp::Add.into(),
+                vec![BinOp::Mul.into(), left.clone(), right.diff(tokens)].into(),
+                vec![BinOp::Mul.into(), left.diff(tokens), right].into(),
+            ]
+            .into(),
+            BinOp::Div => vec![
+                BinOp::Div.into(),
                 // top
-                Token::BinOp(BinOp::Sub),
-                Token::BinOp(BinOp::Mul),
-                left.clone(),
-                right.diff(tokens),
-                Token::BinOp(BinOp::Mul),
-                left.diff(tokens),
-                right.clone(),
+                vec![
+                    BinOp::Sub.into(),
+                    vec![BinOp::Mul.into(), left.clone(), right.diff(tokens)].into(),
+                    vec![BinOp::Mul.into(), left.diff(tokens), right.clone()].into(),
+                ]
+                .into(),
                 // bottom
-                Token::BinOp(BinOp::Mul),
-                right.clone(),
-                right,
-            ]),
+                vec![BinOp::Pow.into(), right, 2.into()].into(),
+            ]
+            .into(),
             BinOp::Pow => match right {
-                Token::Number(n) => {
-                    Token::Expression(vec![Token::BinOp(BinOp::Mul), Token::Number(n - 1), left])
-                }
+                Token::Number(n) => vec![BinOp::Mul.into(), (n - 1).into(), left].into(),
                 _ => unimplemented!(),
             },
         }
@@ -150,26 +161,26 @@ impl Diff for Fun {
     fn diff(&self, tokens: &mut Vec<Token>) -> Token {
         let arg = tokens.pop().unwrap();
 
-        let expr = match self {
-            Fun::Sin => vec![Token::Fun(Fun::Cos), arg],
+        match self {
+            Fun::Sin => vec![Fun::Cos.into(), arg],
             Fun::Cos => vec![
-                Token::BinOp(BinOp::Mul),
-                Token::Number(-1),
-                Token::Expression(vec![Token::Fun(Fun::Sin), arg]),
+                BinOp::Mul.into(),
+                (-1).into(),
+                vec![Fun::Sin.into(), arg].into(),
             ],
             Fun::Tan => vec![
                 Token::BinOp(BinOp::Div),
-                Token::Expression(vec![
-                    Token::BinOp(BinOp::Pow),
-                    Token::Expression(vec![Token::Fun(Fun::Cos), arg]),
-                    Token::Number(2),
-                ]),
+                vec![
+                    BinOp::Pow.into(),
+                    vec![Token::Fun(Fun::Cos), arg].into(),
+                    2.into(),
+                ]
+                .into(),
             ],
-            Fun::Exp => vec![Token::Fun(Fun::Exp), arg],
-            Fun::Ln => vec![Token::BinOp(BinOp::Div), Token::Number(1), arg],
-        };
-
-        Token::Expression(expr)
+            Fun::Exp => vec![Fun::Exp.into(), arg],
+            Fun::Ln => vec![BinOp::Div.into(), 1.into(), arg],
+        }
+        .into()
     }
 }
 
@@ -240,10 +251,10 @@ mod tests {
     fn simple() {
         assert_eq!(diff("5"), "0");
         assert_eq!(diff("x"), "1");
-        assert_eq!(diff("+ x x"), "1");
+        assert_eq!(diff("+ x x"), "2");
         assert_eq!(diff("+ 1 x"), "1");
-        assert_eq!(diff("/ 1 x"), "1");
-        assert_eq!(diff("- 5 + 10 x"), "0");
+        assert_eq!(diff("/ 1 x"), "/ 1 (^ x 2)");
+        assert_eq!(diff("- 5 (+ 10 x)"), "1");
     }
 
     #[test]
